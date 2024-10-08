7d9ab609bcf21f0cf74792b45bfc9300abc7194a
oleole39 <59071673+oleole39@users.noreply.github.com>
2024-10-08 19:07:52 +0200

    first test

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,5 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+
+nodejs_version=20 # mirror https://github.com/AykutSarac/jsoncrack.com/blob/main/.github/workflows/deploy.yml
